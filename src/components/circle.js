490bf6b39a2e517d79da44e27ab96aa632171f75
sunilkushwaha <sunilkushwaha055>
2023-06-21 00:34:52 +0530

    done

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,17 @@
 // import using require
+var Shape = require('./shape.js');
+
 
 // declare class
+class Circle extends Shape{
+    constructor(color){
+        super();
+    }
+    calculateArea(){
 
+    }
+}
+
+module.exports = Circle;
 
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
+class Shape{
+    constructor(){
+    this.color = "green";
+    }
+    drewShape(){
+
+    }
+    calculateArea(){
+
+    }
+}
 
 // export class using module.exports
+module.exports = Shape;
